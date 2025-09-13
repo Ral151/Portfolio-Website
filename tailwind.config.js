82fdae1f559c57affc7787fb9c81cd283523cbc1
Ral151 <gerald.rald19@gmail.com>
2025-09-13 19:51:28 +0800

    test

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      extend: {
+        fontFamily: {
+            'culpa': ['"Mea Culpa"', 'cursive'],
+            'doulaise': ['"Monsieur La Doulaise"', 'cursive'],
+            borderColor: {
+      DEFAULT: '#000000',
+            }
+        }
+      },
+    },
+    plugins: [],
+  }
